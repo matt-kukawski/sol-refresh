636e791bb9f9f6050ad6e94a6cf6dd06582e9cd6
Matt Kukawski <matt@sonoflilith.com>
2021-09-02 17:43:17 +0100

    png link fix

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -55,10 +55,10 @@ function NavBar() {
             <div className="navbar-end is-primary">
 
                   <a href="https://github.com/matt-kukawski" alt="GitHub">
-                      <img className="contact-icon" src={require('images/projects/github-logo.png')} alt="GitHub" />
+                      <img className="contact-icon" src={require('images/projects/github-logo.png').default} alt="GitHub" />
                   </a>
                   <a href="https://www.linkedin.com/in/mattkukawski/" alt="LinkedIn">
-                      <img className="contact-icon" src={require('images/projects/linkedin-logo.png')} alt="LinkedIn" />
+                      <img className="contact-icon" src={require('images/projects/linkedin-logo.png').default} alt="LinkedIn" />
                   </a>
                   <a 
                     onClick={
@@ -67,7 +67,7 @@ function NavBar() {
                     href="mailto:mail@sonoflilith.com" 
                     alt="Email"
                   >
-                    <img className="contact-icon" src={require('images/projects/email-logo.png')} alt="Email" />
+                    <img className="contact-icon" src={require('images/projects/email-logo.png')default} alt="Email" />
                   </a>
             </div>
         </div>
